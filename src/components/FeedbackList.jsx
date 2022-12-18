1a5b7004f8e487ea3d76fa47e1b28742714591a5
Sarthak Rathi <sarthakr85@gmail.com>
2022-12-19 02:36:06 +0530

    Cleared some errors

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -5,7 +5,7 @@ import FeedbackItem from "./FeedbackItem";
 import FeedbackContext from "../context/FeedbackContext";
 
 function FeedbackList() {
-  const { feedback, dele } = useContext(FeedbackContext);
+  const { feedback } = useContext(FeedbackContext);
 
   if (!feedback || feedback.length === 0) {
     return <p>No Feedback Yet</p>;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 function Header({ text, bgColor, textColor }) {
   const headerColor = {
