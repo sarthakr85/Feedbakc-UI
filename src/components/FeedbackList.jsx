59e3228d86c7343d7f52746897955e709a0aff2b
Sarthak Rathi <sarthakr85@gmail.com>
2022-12-26 15:07:17 +0530

    finally done i think

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -3,14 +3,17 @@ import { LazyMotion, domAnimation, m } from "framer-motion";
 import FeedbackItem from "./FeedbackItem";
 // import PropTypes from "prop-types";
 import FeedbackContext from "../context/FeedbackContext";
+import Spinner from "./shared/Spinner";
 
 function FeedbackList() {
-  const { feedback } = useContext(FeedbackContext);
+  const { feedback, isLoading } = useContext(FeedbackContext);
 
-  if (!feedback || feedback.length === 0) {
+  if (!isLoading && (!feedback || feedback.length === 0)) {
     return <p>No Feedback Yet</p>;
   }
-  return (
+  return isLoading ? (
+    <Spinner />
+  ) : (
     <div className="feedback-list">
       <LazyMotion features={domAnimation}>
         {feedback.map((item) => (
--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,50 +1,68 @@
-import { createContext, useState } from "react";
-import { v4 as uuidv4 } from "uuid";
+import { createContext, useState, useEffect } from "react";
 
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState([
-    {
-      id: 1,
-      text: "This item is from context 1",
-      rating: 10,
-    },
-    {
-      id: 2,
-      text: "This item is from context 2",
-      rating: 3,
-    },
-    {
-      id: 3,
-      text: "This item is from context 3",
-      rating: 7,
-    },
-  ]);
-
+  const [isLoading, setIsLoading] = useState(true);
+  const [feedback, setFeedback] = useState([]);
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {},
     edit: false,
   });
 
+  useEffect(() => {
+    fetchFeedback();
+  }, []);
+
+  //Fetch Feedback
+  const fetchFeedback = async () => {
+    const response = await fetch(`/feedback?_sort=id&_order=desc`);
+
+    const data = await response.json();
+    console.log(data);
+
+    setFeedback(data);
+    setIsLoading(false);
+  };
+
   // Delete feedback
-  const deleteFeedback = function (id) {
+  const deleteFeedback = async function (id) {
     if (window.confirm("Are you sure you want to delete?")) {
+      await fetch(`/feedback/${id}`, {
+        method: "DELETE",
+      });
+
       setFeedback(feedback.filter((item) => item.id !== id));
     }
   };
 
   //Add feedback
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4();
-    setFeedback([newFeedback, ...feedback]);
+  const addFeedback = async (newFeedback) => {
+    const response = await fetch(`/feedback`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newFeedback),
+    });
+
+    const data = await response.json();
+    setFeedback([data, ...feedback]);
   };
 
   //Update feedback item
 
-  const updateFeedback = (id, updItem) => {
+  const updateFeedback = async (id, updItem) => {
+    const response = await fetch(`/feedback/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updItem),
+    });
+    const data = await response.json();
     setFeedback(
-      feedback.map((item) => (item.id === id ? { ...item, ...updItem } : item))
+      feedback.map((item) => (item.id === id ? { ...item, ...data } : item))
     );
     setFeedbackEdit({ item: {}, edit: false });
   };
@@ -59,6 +77,7 @@ export const FeedbackProvider = ({ children }) => {
       value={{
         feedback,
         feedbackEdit,
+        isLoading,
         deleteFeedback,
         addFeedback,
         editFeedback,
